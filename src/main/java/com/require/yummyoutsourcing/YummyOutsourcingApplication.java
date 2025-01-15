bbb8f2f6756560f636f0a588d154b15c9b4dca2e
Do-oya <qkrwjdgus1441@naver.com>
2025-01-15 17:54:49 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/require/yummyoutsourcing/YummyOutsourcingApplication.java
@@ -0,0 +1,13 @@
+package com.require.yummyoutsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyOutsourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyOutsourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/require/yummyoutsourcing/YummyOutsourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.require.yummyoutsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyOutsourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
